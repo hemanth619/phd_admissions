097fd97d868bbeb8b5e9fca3f5358dabd9fc06d7
hemanth619 <gsaihemanth619@gmail.com>
2015-04-20 15:48:25 +0530

    changes

--- header_logo.php
+++ header_logo.php
@@ -1,5 +1,9 @@
 <div id="header" class="row" style="margin-bottom: 0px;">
-	<div class="col s2" style="width: auto;"><a href="../index.php"><img id="logo" src="images/iiitdm.svg"></a></div>	
+	<a href="http://www.iiitdm.ac.in/"><div class="col s2" id="instiLogo">
+		<img src="images/iiitdm - inside.svg" id="instiLogoInside"   />
+		<img src="images/iiitdm - ring.svg"  id="instiLogoOutside"  />
+		<!-- <img id="logo" src="images/iiitdm.svg"> -->
+	</div></a>
 	<div class="col s10">
 		<div class="row col s12" style="margin-bottom: 0px;" id="college_name">
 			<span style="font-size: 30px;">Indian Institute of Information Technology, Design & Manufacturing</span>
